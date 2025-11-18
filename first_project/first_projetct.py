523f34a3ca81f7faa1fd4a980563fce35866705b
Henrique Cunha <henriqueartusi39@gmail.com>
2025-11-18 17:36:51 -0300

    Create features to Add, Replace, Read and Clear the file

--- first_project/first_projetct.py
+++ first_project/first_projetct.py
@@ -1,1 +1,25 @@
-print('Hello World!')
+from pathlib import Path
+
+archive = Path('todo_list.txt')#?
+
+print(archive)
+
+def add_task(task):
+
+    with archive.open('a') as open: open.write(task + '\n')
+
+def remove_task():
+    ...
+
+def replace_in_file(previus, new):
+
+    modified_content = archive.read_text().replace(previus, new)
+    archive.write_text(modified_content)
+
+def read_task():
+    print(archive.read_text())
+
+def clear_file():
+    archive.write_text('')
+
+read_task()
